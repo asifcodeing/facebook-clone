a9381c574501d41c6df2726bc7868356f83efad8
faizan <faizan@gmail.com>
2020-12-02 18:44:11 +0530

    facebookclone done

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+            hii
+                <img src="https://www.facebook.com/?ref=logo" alt=""/>
+            </div>
+            <div className="header__middle"></div>
+            <div className="header__right"></div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCeiyhx_XX-OD_1dcX5hlN2_nV4xIypZkM",
+    authDomain: "my-demo-270012.firebaseapp.com",
+    databaseURL: "https://my-demo-270012.firebaseio.com",
+    projectId: "my-demo-270012",
+    storageBucket: "my-demo-270012.appspot.com",
+    messagingSenderId: "1096719025262",
+    appId: "1:1096719025262:web:04120b3d952a759a1a527f",
+    measurementId: "G-H4G73VHJ9Q"
+};
